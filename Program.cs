e0c92137afbe0a53f4978e0a7cedf0817666c61a
Carlos Barron <albertc.barron@gmail.com>
2020-03-05 21:40:03 -0500

    initial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotResourceManipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
